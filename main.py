0ca0841257f0567ae793f8aff4cc22e1510860c5
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-01-10 23:07:52 -0300

    examples: label, button, Entry components

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("Hello world TKinter")
 window.minsize(width=500, height=300)
 
 #label
 
-my_label = tkinter.Label(text="Label abc", font=("Arial", 24))
-my_label.pack(side= "bottom")
+my_label = Label(text="Label abc", font=("Arial", 24))
+my_label.pack()
+
+my_label["text"] = "new text"
+my_label.config(text="text v3")
+
+#button
+
+def button_clicked():
+
+    button.config(text="Done")
+    button.config(state="disabled")
+    user_input = input.get()
+    my_label.config(text=f"Button Confirm clicked + {user_input} ")
+    print(user_input)
 
 
+button = Button(text="Confirm", command=button_clicked)
+button.pack()
 
-
+#Entry component,
+# basically is a input component
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
