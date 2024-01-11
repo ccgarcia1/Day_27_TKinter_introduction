6d67a80721f78b6c908bbc5f5e54029d2ba6f2ef
ccgarcia1 <42050096+ccgarcia1@users.noreply.github.com>
2024-01-10 22:41:41 -0300

    example *args and **kwargs creating a class with **kwargs

--- playground.py
+++ playground.py
@@ -1,4 +1,5 @@
-#unlimited positional arguments
+#unlimited positional arguments *args
+#unlimited arguments are nothing but tuples
 def add(*args):
     sum = 0
     print(args[3])
@@ -7,4 +8,35 @@ def add(*args):
         #print(n)
     return(sum)
 
-print(add(3, 5, 8, 10, 22))
+#print(add(3, 5, 8, 10, 22))
+
+
+#**kwargs many keyworded Arguments
+#keyword arguments are nothing but dictionaries
+def calculate(n, **kwargs):
+    #print(type(kwargs))
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+    #print(kwargs["add"])
+calculate(3, add=3, multiply=5)
+
+
+#example of a class using **kwargs
+
+class Car:
+
+    def __init__(self, **kw):
+        self.make = kw["make"] #if initialize like that and the user doesn't pass the argument
+                               #there will be an error
+        self.model = kw.get("model") #if initialize like that there is no error if
+                                    #the user doesn't put the argument
+
+
+my_car = Car(make="Honda")
+
+print(my_car.model)
+
